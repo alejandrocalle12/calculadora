cd37452dd7120e65ec85fa42ccf80ae2a682fe92
alejandrocalle12 <acalle.ingecomputo@gmail.com>
2026-03-04 20:53:42 -0500

    se agregan multiplicacion y division

--- calculadora.py
+++ calculadora.py
@@ -5,6 +5,8 @@ num2 = float(input("Ingrese el segundo número: "))
 
 print("1. Sumar")
 print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
 
 opcion = input("Seleccione una opción: ")
 
@@ -12,5 +14,12 @@ if opcion == "1":
     print("Resultado:", num1 + num2)
 elif opcion == "2":
     print("Resultado:", num1 - num2)
+elif opcion == "3":
+    print("Resultado:", num1 * num2)
+elif opcion == "4":
+    if num2 != 0:
+        print("Resultado:", num1 / num2)
+    else:
+        print("No se puede dividir por cero")
 else:
     print("Opción inválida")
